bf46b501380960085262b229cd8ecc9545dce2ea
souma tsuda <soumatsuda@soumanoMacBook-Pro.local>
2023-10-13 17:54:59 +0900

    fix a main go

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/sou1991/subject_x_golangs_api_/model"
+	"github.com/sou1991/subject_x_golangs_api_/router"
+	"github.com/sou1991/subject_x_golangs_api_/controller"
+	"github.com/sou1991/subject_x_golangs_api_/repository"
 )
 
-func main() {
-	router := gin.Default()
+var repo IAlbumsRepository = repository.NewAlbumRepository()
+var ctrl IAlbumsController = controller.NewAlbumController(repo)
+var ro IHandlerAlbumsApi = router.NewRouter(ctrl)
 
-	router.GET("/albums", model.GetAlbums)
-	router.GET("/albums/:id", model.GetAlbumByID)
-	router.POST("/albums", model.PostAlbums)
-	router.Run("0.0.0.0:80")
+func main() {
+	r := gin.Default()
+	ro.AlbumsApiCall(&r)
+	r.Run("0.0.0.0:80")
 }
